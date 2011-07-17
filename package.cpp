52b5aa93aacd43d95dd2047830dfef7f36df7282
Jakob Gruber <jakob.gruber@gmail.com>
2011-07-17 21:33:07 +0200

    Line up optdeps correctly in info pane

--- package.cpp
+++ package.cpp
@@ -41,7 +41,7 @@ Package::Package(alpm_pkg_t* pkg, alpm_db_t *localdb) :
 
     _licenses = list2str(alpm_pkg_get_licenses(_pkg), " ");
     _groups = list2str(alpm_pkg_get_groups(_pkg), " ");
-    _optdepends = list2str(alpm_pkg_get_optdepends(_pkg), "\n");
+    _optdepends = list2str(alpm_pkg_get_optdepends(_pkg), "\n            "); /* line up correctly in info pane */
     _conflicts = list2str(alpm_pkg_get_conflicts(_pkg), " ");
     _provides = list2str(alpm_pkg_get_provides(_pkg), " ");
     _replaces = list2str(alpm_pkg_get_replaces(_pkg), " ");
