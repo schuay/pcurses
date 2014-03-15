d4f1c3ef7f300be68c116cb80e980762e2d91620
Jakob Gruber <jakob.gruber@gmail.com>
2014-03-15 17:20:23 +0100

    Provide more extensive help message

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,67 @@
 
 static char *opt_conf_file = nullptr;
 
+static void usage()
+{
+    fprintf(stderr,
+            "Usage: %s [-h] [-v] [-f CONF_FILE]\n"
+            "\n"
+            "Arguments:\n"
+            "----------\n"
+            "-h:            print this message\n"
+            "-v:            print version info\n"
+            "-f:            specify an alternate config file location\n"
+            "\n"
+            "Keyboard shortcuts:\n"
+            "-------------------\n"
+            "esc:           cancel\n"
+            "q:             quit\n"
+            "1 to 0:        hotkeys (as configured in %s.conf)\n"
+            "!:             execute command, replacing %%p with selected package names\n"
+            "@:             run the specified macro (as configured in %s.conf)\n"
+            "%%:             run the specified control command (for example, %%filter_clear)\n"
+            "r:             reload package info\n"
+            "/:             filter packages by specified fields (using regexp)\n"
+            "n:             filter packages by name (using regexp)\n"
+            "c:             clear all package filters\n"
+            "C:             clear the package queue\n"
+            "?:             search packages\n"
+            ".:             sort packages by specified field\n"
+            ";:             colorcode packages by specified field\n"
+            "tab:           switch focus between list and queue panes\n"
+            "left/right:    add/remove packages from the queue\n"
+            "up/down,\n"
+            "pg up/down,\n"
+            "home/end:      navigation\n"
+            "up/down:       browse history while in input mode\n"
+            "\n"
+            "Command syntax:\n"
+            "-------\n"
+            "Filter, search, sort, and colorcode operations all use slight variations of the\n"
+            "same syntax:\n"
+            "\n"
+            "<operator>[[<field list>][!]:]<regex>\n"
+            "\n"
+            "where <operator> is the appropriate operator char (/ for filter),\n"
+            "<field list> is an optional list of fields to search (n for package name)\n"
+            "! negates the query and filters all non-matching packages\n"
+            "and regex specifies the search terms.\n"
+            "\n"
+            "Examples:\n"
+            "\n"
+            "/nc:gnome displays all packages with either name or description matching 'gnome'\n"
+            "/gnome displays the same results (name & desc are default fields)\n"
+            "/nc!:gnome displays the complement to the previous query\n"
+            "\n"
+            "Control commands:\n"
+            "-----------------\n"
+            "The following strings may be used as control commands:\n"
+            "\n"
+            "scroll_up, scroll_down, scroll_home, scroll_end, scroll_pageup,scroll_pagedown,\n"
+            "switch_focus,queue_push,queue_pop,queue_clear,help,quit,reload,filter_clear\n",
+            APPLICATION_NAME, APPLICATION_NAME, APPLICATION_NAME);
+}
+
 static void parseargs(int argc, char *argv[])
 {
     int opt;
@@ -39,7 +100,7 @@ static void parseargs(int argc, char *argv[])
             break;
         case 'h':
         default:
-            fprintf(stderr, "Usage: %s [-h] [-v] [-f CONF_FILE]\n", APPLICATION_NAME);
+            usage();
             exit(EXIT_FAILURE);
         }
     }
